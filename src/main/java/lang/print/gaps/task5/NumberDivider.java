45749fad38d16157fb62af9177b4c89d15c0db34
Vagharshak Samsonyan <vagosamsonyan@yahoo.com>
2024-03-31 11:20:32 +0400

    2 of 3 tests passed

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,8 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    int divide(int toBeDivided) {
+        return (int) Math.floor((double) toBeDivided / 5.0);
     }
 }
+
